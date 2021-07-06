b83d67711c42713c686806a56d033344f2dd3c3b
PrplHrt <PrplHrt@users.noreply.github.com>
2021-07-06 17:10:06 +0000

    deploy: c0bf1033d218505ade2e9fec68e370f526b54b56

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/Unproffesional-Honesty/assets/js/search-data.json', true);
+    request.open('GET', '/Unprofessional-Honesty/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
